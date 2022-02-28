45dfa9925b0cb5d7840609bfab5f6d7837e4bd02
Дмитрий Шелестовский <dashelesto@mts.ru>
2022-02-28 19:17:19 +0300

    :wrench: architecture web-asm

--- src/bobthebuilder/cli/main.py
+++ src/bobthebuilder/cli/main.py
@@ -7,7 +7,7 @@ from bobthebuilder.builder import BobBuilder
 def build_cli():
     args = argparse.ArgumentParser(description="Сборщик проектов на defold")
     args.add_argument('-p', choices=['js-web'], dest='platform', help='Build platform.', required=True)
-    args.add_argument('-a', choices=['js-web'], dest='arch', help='Build Architecture', required=True)
+    args.add_argument('-a', choices=['js-web', 'js-web,wasm-web'], dest='arch', help='Build Architecture', required=True)
     args.add_argument('-d', help='Game project directory')
     args.add_argument('-o', help='Compiled artifact directory', dest='out_dir', required=True)
     parsed = args.parse_args()
